883607e1c7c542e0b5f0c54170879a6c1eebb0d5
Your Name <you@example.com>
2024-12-05 00:09:50 -0500

    add new file

--- GameWithSumpsAndCollisions/GameWithSumpsAndCollisions/obj/Debug/net8.0-windows/GameWithSumpsAndCollisions.AssemblyInfo.cs
+++ GameWithSumpsAndCollisions/GameWithSumpsAndCollisions/obj/Debug/net8.0-windows/GameWithSumpsAndCollisions.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameWithSumpsAndCollisions")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9338087ac7615a3f3687fb6ca770e32801d589a7")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameWithSumpsAndCollisions")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameWithSumpsAndCollisions")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
